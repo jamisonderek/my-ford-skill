4209f8c5e82b1fe56f39937bbdb69acaa76b379f
Derek Jamison <derek@jamisoncreations.com>
2021-07-30 13:11:05 -0500

    Update domain to reflect an ngrok.io domain instead of whitelabel ngrok domain.

--- lambda/settings.js
+++ lambda/settings.js
@@ -1,7 +1,7 @@
 /* eslint-disable linebreak-style */
 
 // TODO: REPLACE THIS WITH YOUR DOMAIN.
-exports.domain = 'https://api.jamisoncreations.com';
+exports.domain = 'https://jamisoncreations.ngrok.io';
 
 // Use https://www.guidgen.com/ or similar site to generate a unique id. 
 exports.userid = '856a89f0-e804-491f-a185-8a9255971a45';
